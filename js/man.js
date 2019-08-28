41c7a94fc528edaa162f66f91691b6c16d956293
kendax <guandarufaith7@gmail.com>
2019-08-28 23:16:25 +0300

    first commit

---
+++ js/man.js
@@ -0,0 +1,67 @@
+jQuery(document).ready(function(){
+	"use strict";
+	$('#header').carouFredSel({
+		responsive:true,
+		width:'100%',
+		circular:true,
+		scroll:{
+				items:1,
+				duration:500,
+				pauseOnHover:true
+			},
+			auto:true,
+			items:{
+					visible:{
+						min:1,
+						max:1
+					},
+					height:"variable"
+				},
+				pagination:{
+					container:"",
+					pageAnchorBuilder:false
+				}		
+
+
+
+	});
+$(window).scroll(function(){
+var top = $(window).scrollTop();
+if(top>=60){
+$("header").addClass('secondary');
+}
+else
+if($("header").hasClass('secondary')){
+$("header").removeClass('secondary');
+}
+});
+$(`#menu`).slicknav({
+	label:``,
+
+})
+
+});
+const navSlide = () => {
+	const burger = document.querySelector('.burger');
+	const nav = document.querySelector('.menu');
+	const navLinks = document.querySelectorAll('.menu li');
+	
+burger.addEventListener('click', () => {
+		//Toggle Nav
+		nav.classList.toggle('nav-active');
+		//Animate Links
+		navLinks.forEach((link, index) => {
+			if(link.style.animation){
+				link.style.animation = '';
+			} else {
+				link.style.animation = `navLinkFade 0.5s ease forwards ${index /7 + 0.5}s`;
+				console.log(index / 7);
+		}
+		});
+		//Burger Animation
+		burger.classList.toggle('toggle');
+	});
+
+}
+
+navSlide();

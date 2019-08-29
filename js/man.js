f32933684d310a9c62b6294eaff84153e7a684fb
kendax <46243718+kendax@users.noreply.github.com>
2019-08-29 15:53:09 +0300

    Update man.js

--- js/man.js
+++ js/man.js
@@ -34,4 +34,28 @@ else
 if($("header").hasClass('secondary')){
 $("header").removeClass('secondary');
 }
-});
+});const navSlide = () => {
+	const burger = document.querySelector('.burger');
+	const nav = document.querySelector('.menu');
+	const navLinks = document.querySelectorAll('.menu li');
+	
+burger.addEventListener('click', () => {
+		//Toggle Nav
+		nav.classList.toggle('nav-active');
+		//Animate Links
+		navLinks.forEach((link, index) => {
+			if(link.style.animation){
+				link.style.animation = '';
+			} else {
+				link.style.animation = `navLinkFade 0.5s ease forwards ${index /7 + 0.5}s`;
+				console.log(index / 7);
+		}
+		});
+		//Burger Animation
+		burger.classList.toggle('toggle');
+	});
+
+}
+
+navSlide();
+	
